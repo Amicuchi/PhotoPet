8fa7273de210bf66fc72db9f7e81718f4a656672
Amicuchi <andersonamicuchi@gmail.com>
2020-12-02 10:59:11 -0300

    alteração da posição do carrossel na index

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    
+    $('#imageGallery').lightSlider({
+        gallery:true,
+        item:1,
+        loop:true,
+        thumbItem:7,
+        slideMargin:0,
+        enableDrag: true,
+        addClass: 'brand-slider-full',
+        currentPagerPosition:'left',
+        adaptiveHeight: true,
+        onSliderLoad: function(el) {
+            el.lightGallery({
+                selector: '#imageGallery .lslide'
+            });
+        }   
+    });
+
+
+});
