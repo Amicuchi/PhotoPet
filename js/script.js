ad31df3d60a99d44e715376ab6941e30e4a9ee8a
Leonardo Meshoes <lmeshoes@gmail.com>
2020-12-11 10:57:39 -0300

    correcao de bugs

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
 $(document).ready(function() {
     
     $('#imageGallery').lightSlider({
-        gallery:true,
+        gallery:false,
         item:1,
         loop:true,
-        thumbItem:7,
         slideMargin:0,
         enableDrag: true,
         addClass: 'brand-slider-full',
         currentPagerPosition:'left',
         adaptiveHeight: true,
-        onSliderLoad: function(el) {
-            el.lightGallery({
-                selector: '#imageGallery .lslide'
-            });
-        }   
+        pager: false
+
     });
 
 
